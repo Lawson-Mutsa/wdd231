175e99bfffa6ec775a84f7d789a4dbb0c907af67
Lawson Mutsa <admin@lawsonmutsa.com>
2025-07-23 19:25:44 +0200

    Test

---
+++ modal.js
@@ -0,0 +1,8 @@
+const modal = document.getElementById('modal-one')
+const button = document.getElementById('button')
+modal.showModal();
+
+closeModal.addEventListener('click',() => {
+    modal.closest();
+
+});
